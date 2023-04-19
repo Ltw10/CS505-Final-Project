2b316fe130e9a0da89c6a6f4ffc7eeeafbca8168
marcod07 <112658948+marcod07@users.noreply.github.com>
2023-04-18 21:07:50 -0400

    Both functions completed

--- opreport.py
+++ opreport.py
@@ -1,32 +1,42 @@
-# Importing subscriber code has not been used yet 
-import sqlitedb
-import subscriber 
+from sqlitedb import create_connection
 import json
 
-def generate_hospital_report(hospital_name):
+def generate_hospital_report(hospital_id):
     # Connect to the sqlite database
-    conn = sqlitedb.connect('cs505relational.db')
+    conn = create_connection()
     
     # Create a cursor object
     c = conn.cursor()
     
     # Execute a query to retrieve the patient status for the given hospital (still working on finishing queries)
-    c.execute(f"SELECT COUNT(*) FROM patients WHERE hospital='{hospital_name}' AND status='in-patient'")
+    c.execute(f"SELECT COUNT(*) FROM hospital_data WHERE hospital_id='{hospital_id}' AND patient_status=1")
     in_patients = c.fetchone()[0]
     
-    c.execute(f"SELECT COUNT(*) FROM patients WHERE hospital='{hospital_name}' AND status='icu'")
+    c.execute(f"SELECT COUNT(*) FROM hospital_data WHERE hospital_id='{hospital_id}' AND patient_status=2")
     icu_patients = c.fetchone()[0]
     
-    c.execute(f"SELECT COUNT(*) FROM patients WHERE hospital='{hospital_name}' AND status='ventilator'")
+    c.execute(f"SELECT COUNT(*) FROM hospital_data WHERE hospital_id='{hospital_id}' AND patient_status=3")
     ventilator_patients = c.fetchone()[0]
-    
-    c.execute(f"SELECT COUNT(*) FROM patients WHERE hospital='{hospital_name}' AND vaccinated='yes' AND status='in-patient'")
+
+    c.execute(f"SELECT patient_mrn FROM hospital_data WHERE hospital_id='{hospital_id}' AND patient_status=1")
+    in_patient_list = c.fetchall()
+
+    query = "select COUNT(*) FROM vax_data WHERE patient_mrn IN {}".format(in_patient_list)
+    c.execute(query)
     vaccinated_in_patients = c.fetchone()[0]
 
-    c.execute(f"SELECT COUNT(*) FROM patients WHERE hospital='{hospital_name}' AND vaccinated='yes' AND status='icu'")
+    c.execute(f"SELECT patient_mrn FROM hospital_data WHERE hospital_id='{hospital_id}' AND patient_status=2")
+    icu_patient_list = c.fetchall()
+
+    query = "select COUNT(*) FROM vax_data WHERE patient_mrn IN {}".format(icu_patient_list)
+    c.execute(query)
     vaccinated_icu_patients = c.fetchone()[0]
 
-    c.execute(f"SELECT COUNT(*) FROM patients WHERE hospital='{hospital_name}' AND vaccinated='yes' AND status='ventilator'")
+    c.execute(f"SELECT patient_mrn FROM hospital_data WHERE hospital_id='{hospital_id}' AND patient_status=3")
+    ventilator_patient_list = c.fetchall()
+
+    query = "select COUNT(*) FROM vax_data WHERE patient_mrn IN {}".format(ventilator_patient_list)
+    c.execute(query)
     vaccinated_vent_patients = c.fetchone()[0]
     
     # Calculate the percentage of vaccinated patients
@@ -44,12 +54,67 @@ def generate_hospital_report(hospital_name):
         "patient_vent_vax": percent_vaccinated_ventilator
     }
     
-    # Convert the dictionary to a JSON string
-    json_data = json.dumps(report_data)
+    # Close the connection
+    conn.close()
+
+    # Return data
+    return(report_data)
+
+def generate_overall_report():
+    # Connect to the sqlite database
+    conn = create_connection()
     
-    # Print the JSON string to the console
-    print(json_data)
+    # Create a cursor object
+    c = conn.cursor()
+    
+    # Execute a query to retrieve the patient status for the given hospital (still working on finishing queries)
+    c.execute(f"SELECT COUNT(*) FROM hospital_data WHERE patient_status=1")
+    in_patients = c.fetchone()[0]
+    
+    c.execute(f"SELECT COUNT(*) FROM hospital_data WHERE patient_status=2")
+    icu_patients = c.fetchone()[0]
+    
+    c.execute(f"SELECT COUNT(*) FROM hospital_data WHERE patient_status=3")
+    ventilator_patients = c.fetchone()[0]
+
+    c.execute(f"SELECT patient_mrn FROM hospital_data WHERE patient_status=1")
+    in_patient_list = c.fetchall()
+
+    query = "select COUNT(*) FROM vax_data WHERE patient_mrn IN {}".format(in_patient_list)
+    c.execute(query)
+    vaccinated_in_patients = c.fetchone()[0]
+
+    c.execute(f"SELECT patient_mrn FROM hospital_data WHERE patient_status=2")
+    icu_patient_list = c.fetchall()
+
+    query = "select COUNT(*) FROM vax_data WHERE patient_mrn IN {}".format(icu_patient_list)
+    c.execute(query)
+    vaccinated_icu_patients = c.fetchone()[0]
+
+    c.execute(f"SELECT patient_mrn FROM hospital_data WHERE patient_status=3")
+    ventilator_patient_list = c.fetchall()
+
+    query = "select COUNT(*) FROM vax_data WHERE patient_mrn IN {}".format(ventilator_patient_list)
+    c.execute(query)
+    vaccinated_vent_patients = c.fetchone()[0]
+    
+    # Calculate the percentage of vaccinated patients
+    percent_vaccinated_in = (vaccinated_in_patients / in_patients)
+    percent_vaccinated_icu = (vaccinated_icu_patients / icu_patients)
+    percent_vaccinated_ventilator = (vaccinated_vent_patients / ventilator_patients)
+    
+    # Create a dictionary with the report data
+    report_data = {
+        "in-patient_count": in_patients,
+        "in-patient_vax": percent_vaccinated_in,
+        "icu-patient_count": icu_patients,
+        "icu_patient_vax": percent_vaccinated_icu,
+        "patient_vent_count": ventilator_patients,
+        "patient_vent_vax": percent_vaccinated_ventilator
+    }
     
     # Close the connection
     conn.close()
 
+    # Return data
+    return(report_data)
